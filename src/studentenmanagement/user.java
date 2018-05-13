f0307cafb9284b3594f22e75ef80eb9ea168b782
domin <domin@172.17.33.99>
2018-05-13 12:38:42 +0300

    düzenleme

---
+++ src/studentenmanagement/user.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentenmanagement;
+
+/**
+ *
+ * @author domin
+ */
+public class user {
+    private int ID;
+    private String Vorname,Nachname,Benutzername,Passwort,Tel,Email;
+    
+    public user(int ID, String Vorname,String Nachname,String Benutzername,String Passwort,String Tel,String Email) {
+        this.ID = ID;
+        this.Vorname = Vorname;
+        this.Nachname = Nachname;
+        this.Benutzername = Benutzername;
+        this.Passwort = Passwort;
+        this.Tel = Tel;
+        this.Email = Email;  
+    }
+    
+   
+     public int getID(){
+        return ID;
+    }
+
+    public String getVorname(){
+     return Vorname;
+    }
+    public String getNachname(){
+     return Nachname;
+    }
+    public String getBenutzername(){
+     return Benutzername;
+    }
+        public String getPasswort(){
+    return Passwort;
+    }
+    public String getTel(){
+     return Tel;
+    }
+    public String getEmail(){
+    return Email;
+    }
+}
